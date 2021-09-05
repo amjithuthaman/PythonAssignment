d1dbd2a496d61ae6441dfbc52ebb8f57e7587c4a
amjithk <amjithk@outlook.com>
2021-09-05 15:22:18 +0530

    create user

--- profiler/users.py
+++ profiler/users.py
@@ -1,11 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from werkzeug.security import generate_password_hash
+from .db import get_db
 
 blueprint = Blueprint("users", __name__, url_prefix="/users")
 
 
 @blueprint.route("/", methods=["POST"])
 def create_user():
-    return {"action": "user created"}, 201
+    data = request.json
+
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    email = data.get("email")
+    password = data.get("password")
+
+    if not all([first_name, last_name, email, password]):
+        return {"error": "Required fields are missing"}, 400
+
+    db = get_db()
+    hashed_password = generate_password_hash(password)
+    try:
+        cursor = db.execute(
+            "INSERT INTO user (first_name, last_name, email, password) VALUES (?, ?, ?, ?)",
+            (first_name, last_name, email, hashed_password),
+        )
+        db.commit()
+    except db.IntegrityError:
+        return {"error": "This email is already registered"}, 400
+
+    return jsonify({"id": cursor.lastrowid}), 201
 
 
 @blueprint.route("/", methods=["GET"])
