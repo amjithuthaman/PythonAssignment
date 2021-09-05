2a16c38d4e2f4e6eae1fa00258aea693774b8260
amjithk <amjithk@outlook.com>
2021-09-05 15:16:21 +0530

    User Blueprint
    Sample user blue prints

---
+++ profiler/users.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+
+blueprint = Blueprint("users", __name__, url_prefix="/users")
+
+
+@blueprint.route("/", methods=["POST"])
+def create_user():
+    return {"action": "user created"}, 201
+
+
+@blueprint.route("/", methods=["GET"])
+def list_users():
+    return {"action": "list all users"}
+
+
+@blueprint.route("/<int:user_id>/", methods=["GET"])
+def get_user(user_id):
+    return {"action": f"get user {user_id}"}
+
+
+@blueprint.route("/<int:user_id>/", methods=["PUT"])
+def update_user(user_id):
+    return {"action": f"update user {user_id}"}
+
+
+@blueprint.route("/<int:user_id>/", methods=["DELETE"])
+def delete_user(user_id):
+    return "", 204
